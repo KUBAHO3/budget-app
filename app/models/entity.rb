e794824e62dddcd7d825f2e8684b54acb2fd0cc2
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-07 20:28:16 +0530

    Align models

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,5 @@
 class Entity < ApplicationRecord
   belongs_to :user
-  has_many :group
+  has_many :group_entities, dependent: :destroy
+  has_many :groups, through: :group_entities
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
     belongs_to :user
-    has_many :entity
+    has_many :group_entities, dependent: :destroy
+    has_many :entities, through: :group_entities
 end
