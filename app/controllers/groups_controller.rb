59f1e8a3966a6abc0ac201743c40815968e465b2
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-07 23:45:25 +0530

    Update groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,68 +1,34 @@
-class GroupsController < ApplicationController
-  before_action :set_group, only: %i[show edit update destroy]
+# frozen_string_literal: true
 
-  # GET /groups or /groups.json
+class GroupsController < ApplicationController
   def index
-    @groups = Group.all
+    @groups = current_user.groups.order(created_at: :desc).all
+    @total_amount = []
+
+    @groups.each do |group|
+      total = 0
+      group.group_entities.each do |group_entity|
+        total += group_entity.entity.amount
+      end
+      @total_amount.push(total)
+    end
   end
 
-  # GET /groups/1 or /groups/1.json
-  def show; end
-
-  # GET /groups/new
   def new
     @group = Group.new
   end
 
-  # GET /groups/1/edit
-  def edit; end
-
-  # POST /groups or /groups.json
   def create
-    @group = Group.new(group_params)
-
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /groups/1 or /groups/1.json
-  def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /groups/1 or /groups/1.json
-  def destroy
-    @group.destroy
-
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
-      format.json { head :no_content }
+    @new_group = current_user.groups.new(group_params)
+    if @new_group.save
+      redirect_to groups_path, flash: { alert: 'Your category is registered' }
+    else
+      redirect_to new_group_path, flash: { alert: 'Could not register your category' }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_group
-    @group = Group.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def group_params
     params.require(:group).permit(:name, :icon)
   end
