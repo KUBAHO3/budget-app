fa800ccf7ec28f897b0b440f035561838311530d
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-08 19:44:28 +0530

    Fix rubocop errors

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-# rubocop:disable Metrics/BlockLength
 RSpec.feature 'Logins', type: :feature do
   background { visit new_user_session_path }
   scenario 'displays email field' do
@@ -29,5 +28,4 @@ RSpec.feature 'Logins', type: :feature do
       expect(page).to have_content 'Invalid Email or password.'
     end
   end
-  # rubocop:enable Metrics/BlockLength
 end
