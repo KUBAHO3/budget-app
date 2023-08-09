a2b1d203c721b7b77eeb070744c0378f9f98d7f5
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-09 14:20:23 +0530

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :entities
   has_many :groups
